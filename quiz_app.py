96c6cbdb04d299bf4a27be9577592ae2b548e2ae
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2025-11-02 08:27:28 +0530

    Completed -> To-do Application. Wokring on -> Quiz Application.

--- quiz_app.py
+++ quiz_app.py
@@ -1,14 +1,81 @@
 from tkinter import *
+from tkinter import ttk
+
+"""
+Main window object creation and its configurations.
+GM -> Pack.
+"""
 
 window = Tk()
-window.title("My Quiz")
 window.geometry("600x600")
+window.title("Quiz Application.")
 
-# Header frame
-header_frame = Frame(window, bg="#000000")
-header_frame.pack()
+FONT = "Times New Roman"
+TEXT_BG = "#000000"
+style = ttk.Style()
+style.configure("Custom.TCheckbutton", font=(FONT, 16))
 
-title_label = Label(text="Quiz Application.", font=("Arial", 24))
+"""
+Frame for the header.
+GM -> Grid.
+"""
+header_frame = ttk.Frame(window)
+header_frame.pack(fill="x", padx=10, pady=10)
+
+header_frame.grid_columnconfigure(0, weight=1)
+header_frame.grid_columnconfigure(1, weight=0)
+header_frame.grid_rowconfigure(0, weight=1)
+
+header_label = ttk.Label(header_frame, text="MY QUIZ APPLICATION", font=(FONT, 20))
+header_label.grid(row=0, column=0, sticky="w")
+
+score_label = ttk.Label(header_frame, text="Score: 0", font=(FONT, 14))
+score_label.grid(row=0, column=1)
+
+
+"""
+MCQ Frame.
+GM -> Pack
+"""
+
+questions_options_dictionary = {
+    "que 1" : {
+        "What is the capital of Canada?" : ["Toronto", "b", "c", "d"]
+    },
+    "que 2" : {
+        "What is it?" : ["a", "b", "c", "d"]
+    }
+}
+
+mcq_frame = ttk.Frame(window)
+mcq_frame.pack(fill="x", padx=10, pady=10)
+
+def render_questions_frame():
+
+    questions_frame = ttk.Frame(mcq_frame, height=80)
+    questions_frame.pack_propagate(False)
+    questions_frame.pack(fill="x")
+
+    question_label = ttk.Label(questions_frame, anchor="w", text="Place holder here.", font=(FONT, 18))
+    question_label.pack(fill="x")
+
+    answers_frame = ttk.Frame(mcq_frame)
+    answers_frame.pack(fill="x", expand=True)
+
+    option_1 = ttk.Checkbutton(answers_frame, text="Option 1", style="Custom.TCheckbutton")
+    option_2 = ttk.Checkbutton(answers_frame, text="Option 2", style="Custom.TCheckbutton")
+    option_3 = ttk.Checkbutton(answers_frame, text="Option 3", style="Custom.TCheckbutton")
+    option_4 = ttk.Checkbutton(answers_frame, text="Option 4", style="Custom.TCheckbutton")
+
+    option_1.pack(fill="x")
+    option_2.pack(fill="x")
+    option_3.pack(fill="x")
+    option_4.pack(fill="x")
+
+render_questions_frame()
+
+
+
 
 # Start the main loop
 window.mainloop()
--- to_do_list.py
+++ to_do_list.py
@@ -5,6 +5,7 @@ from tkinter.simpledialog import askstring
 window = Tk()
 window.geometry("600x600")
 window.title("My To-do Application.")
+window.configure(bg="#e6e6ff")
 
 # Window grid column configures.
 window.grid_columnconfigure(0, weight=1)
@@ -15,10 +16,37 @@ window.grid_rowconfigure(1, weight=1)
 # Global variables.
 task_count=0
 
+def handle_on_toggle(check_button, var):
+    if var.get() == 1:
+        check_button.config(font=("Arial", 20, "overstrike"))
+    else:
+        check_button.config(font=("Arial", 20))
+
+def handle_delete(task_row):
+    task_row.destroy()
+
 def add_task_ui(text):
-    task_frame.grid_rowconfigure(0, weight=1)
-    check_button = Checkbutton(task_frame, text=text, anchor="w", font=("Arial", 20))
-    check_button.pack(anchor="w", pady=5, fill="x", expand=False)
+    task_row = Frame(task_frame, bg="#000000")
+    task_row.pack(fill="x", expand=False, pady=5, padx=10)
+
+    var = IntVar()
+
+    check_button = Checkbutton(
+        task_row,
+        text=text,
+        anchor="w",
+        font=("Arial", 20),
+        variable=var,
+        command=lambda: handle_on_toggle(check_button, var),
+        bg="#000000",
+        fg="white",
+        selectcolor="#000000"
+    )
+    check_button.pack(side="left", fill="x", expand=True, padx=10, pady=5)
+
+    delete_button = Button(task_row, text="Delete", command= lambda: handle_delete(task_row))
+    delete_button.pack(side="right", padx=10, pady=5)
+
 
 def handle_add_task(task):
     global task_count
@@ -37,27 +65,35 @@ def take_input():
 
 
 # Header frame to hold the header objects.
-header_frame = Frame(window, width=600)
+header_frame = Frame(window, width=600, bg="#e6e6ff")
 header_frame.grid(row=0, column=0, sticky="ew", pady=10)
 header_frame.grid_columnconfigure(0, weight=1)
 
-header_label = Label(header_frame, text="To-do List", font=("Arial", 24))
+header_label = Label(header_frame, text="To-do List", bg="#e6e6ff", font=("Arial", 24))
 header_label.grid(row=0, column=0, padx=10)
 
 # Task menu.
-task_frame = Frame(window, width=600, bg="#ffffff")
+task_frame = Frame(window, width=600, bg="#e6e6ff")
 task_frame.grid(row=1, column=0, sticky="nsew")
 
 # Footer frame.
-footer_frame = Frame(window, width=600, bg="#f3f3f3", )
+footer_frame = Frame(window, width=600, bg="#e6e6ff")
 footer_frame.grid(row=2, column=0, sticky="nsew")
-footer_frame.grid_columnconfigure(0, weight=1)
-# Task Entry.
-task_entry = Entry(footer_frame, font=("Arial", 20))
-task_entry.grid(ipady=10, row=2, column=0, sticky="ew")
 
-add_task_button = Button(footer_frame, text="+", width=4, font=("Arial", 16), command=take_input)
-add_task_button.grid(row=2, column=1, sticky="e", ipady=10)
+footer_frame.grid_columnconfigure(0, weight=1)
+footer_frame.grid_columnconfigure(1, weight=0)
+footer_frame.grid_rowconfigure(0, weight=1)
+
+task_entry = Entry(footer_frame, font=("Arial", 20))
+task_entry.grid(row=0, column=0, sticky="nsew")
+
+add_task_button = Button(
+    footer_frame,
+    text="+",
+    font=("Arial", 20, "bold"),
+    command=take_input
+)
+add_task_button.grid(row=0, column=1, sticky="nsew")
 
 
 # Start the main loop.
