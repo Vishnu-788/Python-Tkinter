b4369b708a1d0dc60f3c8b573c37ce2818b9eb96
Vishnu Prasad J S <prasadvishnu788@gmail.com>
2025-11-07 09:52:36 +0530

    Music player complete.

--- music_player.py
+++ music_player.py
@@ -1,25 +1,53 @@
 import tkinter as tk
-from tkinter import ttk
-from tkinter.constants import HORIZONTAL
+from tkinter import ttk, filedialog
+import pygame
 
 window = tk.Tk()
 window.geometry("600x300")
 window.title('Music Player')
 
-def handle_play_pause():
-    pass
+pygame.mixer.init()
 
-def handle_prev():
-    pass
+def handle_play():
+    pygame.mixer.music.play()
 
-def handle_next():
-    pass
+def handle_pause():
+    pygame.mixer.music.pause()
 
-progress_bar_frame = ttk.Frame(window)
-progress_bar_frame.pack(fill="x", expand=True, padx=20)
+def handle_resume():
+    pygame.mixer.music.unpause()
 
-progress_bar = ttk.Progressbar(progress_bar_frame, orient="horizontal", length=100, mode="determinate")
-progress_bar.pack(fill="x")
+scale_bar_frame = ttk.Frame(window)
+scale_bar_frame.pack(fill="x", expand=True, padx=20)
+
+def ask_filename():
+    file_path = filedialog.askopenfilename(
+        title="Select music files",
+        filetypes=[("Audio Files", "*.mp3 .wav .ogg")]
+    )
+    return file_path
+
+def load_music():
+    file_path = ask_filename()
+    if file_path:
+        current_track.set(file_path.split("/")[-1])
+        pygame.mixer.music.load(file_path)
+
+current_track = tk.StringVar(value="No track selected.")
+
+current_track_label = ttk.Label(scale_bar_frame, textvariable=current_track)
+current_track_label.pack(fill="x", pady=10)
+
+
+progress_value = tk.DoubleVar()
+scale = ttk.Scale(
+    scale_bar_frame,
+    orient="horizontal",
+    from_=0,
+    to=100,
+    variable=progress_value
+)
+scale.pack(fill="x")
 
 music_controls_frame = ttk.Frame(window)
 music_controls_frame.pack(fill='x', side="bottom", pady=20)
@@ -28,13 +56,16 @@ music_controls_frame.grid_columnconfigure(0, weight=1)
 music_controls_frame.grid_columnconfigure(1, weight=1)
 music_controls_frame.grid_columnconfigure(2, weight=1)
 
-prev_button = ttk.Button(music_controls_frame, text="Previous")
-prev_button.grid(row=0, column=0, padx=10)
+pause_button = ttk.Button(music_controls_frame, text="Pause", command=handle_pause)
+pause_button.grid(row=0, column=0, padx=10)
 
-play_button = ttk.Button(music_controls_frame, text="Play")
+play_button = ttk.Button(music_controls_frame, text="Play", command=handle_play)
 play_button.grid(row=0, column=1, padx=10)
 
-next_button = ttk.Button(music_controls_frame, text="Next")
-next_button.grid(row=0, column=2, padx=10)
+resume_button = ttk.Button(music_controls_frame, text="Resume", command=handle_resume)
+resume_button.grid(row=0, column=2, padx=10)
+
+load_music()
+
 
 window.mainloop()
